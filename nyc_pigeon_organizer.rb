1e84047aa461de4c3ed2704ad863155e0a846535
Mez Charney <mez.charney@gmail.com>
2019-09-27 23:34:40 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,6 +3,9 @@ def nyc_pigeon_organizer(data)
   data.each do |attribute_name, attributes|
     attributes.each do |attributes_value, pigeon_names|
       pigeon_names.each do |name|
-    if new_pigeon_hash[name] = 
-    
+    new_pigeon_hash[name] || = {}
+      end
+    end
+  end
+  new_pigeon_hash
 end
